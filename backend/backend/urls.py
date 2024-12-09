c801e5264b89961bd1511212fd4b9f36be5386ef
Jose Manuel Lopez <jmlopez651@outlook.com>
2024-12-09 19:26:45 +0100

    First steps jwt autenthication

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from api.views import CreateUserView
+from rest_framework_simplejwt import TokenObtainPairView,TokenRefrehView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("api/user/register/", CreateUserView.as_view(),name="register"),
+    path("api/token/", TokenObtainPairView.as_view(), name="get_token"),
+    path("api/token/refresh/", TokenRefrehView.as_view(), name="refresh"),
+    path("api-auth/", include("rest_framework.urls"))
 ]
